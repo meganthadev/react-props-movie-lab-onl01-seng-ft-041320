302705ae90ff8914a5516dbbc823d5c218c95132
Megan Horsfield <meganthadev@gmail.com>
2020-09-29 06:20:30 +0000

    Automatically backed up by Learn

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -4,7 +4,11 @@ export default class CardFront extends Component {
 
   render() {
     return (
+<<<<<<< HEAD
       <div className="card-front" style={{backgroundImage: `url(${this.props.poster})`}}>
+=======
+      <div className="card-front" style={{backgroundImage: 'url (${prop})`}}>
+>>>>>>> 0c0c3400a7f214c1da35754471b3a787cc2c5f9e
       </div>
     )
   }
